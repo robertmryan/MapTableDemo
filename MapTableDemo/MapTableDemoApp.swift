45e474691d5db538c2c5c903993d3e1cf882ffb4
Robert Ryan <robert.ryan@mindspring.com>
2023-11-30 10:19:01 -0800

    Initial Commit

---
+++ MapTableDemo/MapTableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTableDemoApp.swift
+//  MapTableDemo
+//
+//  Created by Robert Ryan on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MapTableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
